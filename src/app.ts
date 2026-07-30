f436d79e4810dc793a95e44f775c301ae93c3fee
Fahad Sarker <moajfahad@gmail.com>
2026-07-30 17:46:36 +0600

    appliaction module, database tabel all application segmaent delted

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import env from "./config/env";
 import AuthRoute from "./modules/Auth/Auth.route";
 import ServiceRoute from "./modules/Services/Services.route";
 import CategoriesRoute from "./modules/categories/categories.route";
-import ApplicationsRoute from "./modules/Applications/Applications.route";
 import TechniciansRoute from "./modules/technicians/technicians.route";
 import ReviewRoute from "./modules/review/review.route";
 import BookingsRoute from "./modules/bookings/bookings.route";
@@ -31,7 +30,6 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/auth", AuthRoute);
 app.use("/api/services", ServiceRoute);
 app.use("/api/categories", CategoriesRoute);
-app.use("/api/applications", ApplicationsRoute);
 app.use("/api/technicians", TechniciansRoute);
 app.use("/api/reviews", ReviewRoute);
 app.use("/api/bookings", BookingsRoute);
