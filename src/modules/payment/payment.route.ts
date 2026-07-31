3144c4e3ddfc5e941e03ad558c05ed58b3de4571
Fahad Sarker <moajfahad@gmail.com>
2026-07-31 18:05:13 +0600

    payment roles add

--- src/modules/payment/payment.route.ts
+++ src/modules/payment/payment.route.ts
@@ -1,12 +1,14 @@
 import {Router} from "express";
 import { paymentController } from "./payment.controller";
+import isAuthinticated from "../../middleware/isAuthinticated";
+import { UserRole } from "../../../generated/prisma/enums";
 const router = Router();
 
-router.post("/create/", paymentController.createPayment);
+router.post("/create/",isAuthinticated(UserRole.CUSTOMER) ,paymentController.createPayment);
 router.post("/webhook", paymentController.handleStripeWebhook);
-router.post("/confirm", paymentController.confirmPayment);
-router.get("/", paymentController.getPayments);
-router.get("/:paymentId", paymentController.getPaymentById);
+router.post("/confirm", isAuthinticated(UserRole.CUSTOMER), paymentController.confirmPayment);
+router.get("/", isAuthinticated(UserRole.CUSTOMER), paymentController.getPayments);
+router.get("/:paymentId", isAuthinticated(UserRole.CUSTOMER), paymentController.getPaymentById);
 
 
 export default router;
